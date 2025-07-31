44641121e81acc04e4c977d1c6edba8eb4c48062
Documenter.jl <documenter@juliadocs.github.io>
2025-07-31 02:33:29 +0000

    build based on a10dbcf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#shorthand_detector","page":"Home","title":"shorthand_detector","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for shorthand_detector.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
